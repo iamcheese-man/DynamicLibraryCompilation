f3e31b3f38e1950dd820afef6a26dd9ea2c884d1
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-02-28 18:08:01 +0000

    Update Bluebutton.m

--- src/Bluebutton.m
+++ src/Bluebutton.m
@@ -1,26 +1,20 @@
-// Tweak.m
-// Simple Objective-C Tweak - Adds a button to every view controller
-
+// Bluebutton.m
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
+#import <objc/message.h>
 
-// Swizzled method
 void swizzled_viewDidAppear(id self, SEL _cmd, BOOL animated) {
-    // Call original implementation
     ((void (*)(id, SEL, BOOL))objc_msgSend)(self, @selector(original_viewDidAppear:), animated);
     
     UIViewController *vc = (UIViewController *)self;
     
-    // Create button
     UIButton *tweakButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [tweakButton setTitle:@"Tweaked! 🎉" forState:UIControlStateNormal];
     tweakButton.backgroundColor = [UIColor systemBlueColor];
     [tweakButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     tweakButton.layer.cornerRadius = 10;
     tweakButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [tweakButton addTarget:vc 
-                    action:@selector(tweakButtonTapped) 
-          forControlEvents:UIControlEventTouchUpInside];
+    [tweakButton addTarget:vc action:@selector(tweakButtonTapped) forControlEvents:UIControlEventTouchUpInside];
     
     [vc.view addSubview:tweakButton];
     
@@ -32,44 +26,24 @@ void swizzled_viewDidAppear(id self, SEL _cmd, BOOL animated) {
     ]];
 }
 
-// Button tap handler
 void tweakButtonTapped(id self, SEL _cmd) {
     UIViewController *vc = (UIViewController *)self;
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Hello!" 
-                                                                   message:@"ObjC tweak button tapped!" 
-                                                            preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Hello!" message:@"ObjC tweak button tapped!" preferredStyle:UIAlertControllerStyleAlert];
     [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
     [vc presentViewController:alert animated:YES completion:nil];
 }
 
-// Constructor - runs when dylib loads
 __attribute__((constructor))
 static void initialize() {
     NSLog(@"[Tweak] Loading...");
     
-    // Add button tap method to UIViewController
-    class_addMethod([UIViewController class], 
-                   @selector(tweakButtonTapped), 
-                   (IMP)tweakButtonTapped, 
-                   "v@:");
+    class_addMethod([UIViewController class], @selector(tweakButtonTapped), (IMP)tweakButtonTapped, "v@:");
     
-    // Swizzle viewDidAppear
-    Method originalMethod = class_getInstanceMethod([UIViewController class], 
-                                                    @selector(viewDidAppear:));
-    Method swizzledMethod = class_getInstanceMethod([UIViewController class], 
-                                                    @selector(original_viewDidAppear:));
+    Method originalMethod = class_getInstanceMethod([UIViewController class], @selector(viewDidAppear:));
     
-    // Add original_viewDidAppear as an alias
-    class_addMethod([UIViewController class],
-                   @selector(original_viewDidAppear:),
-                   method_getImplementation(originalMethod),
-                   method_getTypeEncoding(originalMethod));
+    class_addMethod([UIViewController class], @selector(original_viewDidAppear:), method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
     
-    // Replace viewDidAppear with our swizzled version
-    class_replaceMethod([UIViewController class],
-                       @selector(viewDidAppear:),
-                       (IMP)swizzled_viewDidAppear,
-                       method_getTypeEncoding(originalMethod));
+    class_replaceMethod([UIViewController class], @selector(viewDidAppear:), (IMP)swizzled_viewDidAppear, method_getTypeEncoding(originalMethod));
     
     NSLog(@"[Tweak] Loaded!");
 }
