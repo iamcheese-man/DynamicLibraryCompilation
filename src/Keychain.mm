91486642b51e27fc840a058ec26d0b9d300f7ad4
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-03-15 17:50:50 +0000

    Update Keychain.mm

--- src/Keychain.mm
+++ src/Keychain.mm
@@ -11,54 +11,51 @@ static void showAlert(NSString *title, NSString *message) {
             }
         }
         if (!window) return;
-
         UIAlertController *alert = [UIAlertController
             alertControllerWithTitle:title
             message:message
             preferredStyle:UIAlertControllerStyleAlert];
-
         [alert addAction:[UIAlertAction actionWithTitle:@"Copy" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
             [UIPasteboard generalPasteboard].string = message;
         }]];
         [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
-
         [window.rootViewController presentViewController:alert animated:YES completion:nil];
     });
-}
-
-static void dumpKeychainEntry(NSString *service, NSString *account) {
-    NSDictionary *query = @{
-        (__bridge id)kSecClass:            (__bridge id)kSecClassGenericPassword,
-        (__bridge id)kSecAttrService:      service,
-        (__bridge id)kSecAttrAccount:      account,
-        (__bridge id)kSecReturnData:       @YES,
-        (__bridge id)kSecMatchLimit:       (__bridge id)kSecMatchLimitOne,
-    };
-
-    CFTypeRef result = NULL;
-    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
-
-    if (status == errSecSuccess && result) {
-        NSData *data = (__bridge_transfer NSData *)result;
-        NSString *value = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        if (!value) {
-            value = [data base64EncodedStringWithOptions:0];
-        }
-        showAlert([NSString stringWithFormat:@"Keychain: %@/%@", service, account], value);
-    } else {
-        showAlert(@"Keychain Miss", [NSString stringWithFormat:@"service=%@ account=%@ status=%d", service, account, (int)status]);
-    }
 }
 
 __attribute__((constructor))
 static void init(void) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)),
                    dispatch_get_main_queue(), ^{
-        dumpKeychainEntry(@"com.gloop.deltamobile", @"deviceUUID");
-    });
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)),
-                   dispatch_get_main_queue(), ^{
-        dumpKeychainEntry(@"com.gloop.deltamobile", @".deviceUUID");
+        // Dump ALL generic password keychain items
+        NSDictionary *query = @{
+            (__bridge id)kSecClass:        (__bridge id)kSecClassGenericPassword,
+            (__bridge id)kSecReturnAttributes: @YES,
+            (__bridge id)kSecReturnData:   @YES,
+            (__bridge id)kSecMatchLimit:   (__bridge id)kSecMatchLimitAll,
+        };
+
+        CFTypeRef result = NULL;
+        OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
+
+        if (status == errSecSuccess && result) {
+            NSArray *items = (__bridge_transfer NSArray *)result;
+            NSMutableString *output = [NSMutableString string];
+            for (NSDictionary *item in items) {
+                NSString *service = item[(__bridge id)kSecAttrService] ?: @"(nil)";
+                NSString *account = item[(__bridge id)kSecAttrAccount] ?: @"(nil)";
+                NSData *data = item[(__bridge id)kSecValueData];
+                NSString *value = @"(nil)";
+                if (data) {
+                    value = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                    if (!value) value = [data base64EncodedStringWithOptions:0];
+                }
+                [output appendFormat:@"svc=%@ acct=%@\nval=%@\n\n", service, account, value];
+            }
+            showAlert(@"All Keychain Items", output.length ? output : @"(empty)");
+        } else {
+            showAlert(@"Keychain Dump", [NSString stringWithFormat:@"status=%d (no items or error)", (int)status]);
+        }
     });
 }
