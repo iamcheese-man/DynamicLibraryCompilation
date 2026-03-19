9acb526852a8e547dfa0e227efdd9e29aefc5775
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-03-19 21:13:55 +0000

    Update ToSAdBypass.mm

--- src/ToSAdBypass.mm
+++ src/ToSAdBypass.mm
@@ -1,6 +1,7 @@
 // ToSAdBypass.mm
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 #import <objc/runtime.h>
 #import <objc/message.h>
 
@@ -10,192 +11,175 @@ typedef NS_ENUM(NSInteger, UnityAdsFinishState) {
     kUnityShowCompletionStateCompleted = 2,
 };
 
-typedef NS_ENUM(NSInteger, UnityAdsShowError) {
-    kUnityShowErrorNotInitialized   = 0,
-    kUnityShowErrorNotReady         = 1,
-    kUnityShowErrorVideoPlayerError = 2,
-    kUnityShowErrorInvalidArgument  = 3,
-    kUnityShowErrorNoConnection     = 4,
-    kUnityShowErrorAlreadyShowing   = 5,
-    kUnityShowErrorInternalError    = 6,
-};
-
 @protocol UnityAdsShowDelegate <NSObject>
 @optional
 - (void)unityAdsShowComplete:(NSString *)placementId withFinish:(UnityAdsFinishState)state;
-- (void)unityAdsShowFailed:(NSString *)placementId withError:(UnityAdsShowError)error withMessage:(NSString *)message;
 - (void)unityAdsShowStart:(NSString *)placementId;
-- (void)unityAdsShowClick:(NSString *)placementId;
 @end
 
 @protocol UnityAdsLoadDelegate <NSObject>
 @optional
 - (void)unityAdsAdLoaded:(NSString *)placementId;
-- (void)unityAdsAdFailedToLoad:(NSString *)placementId withError:(NSInteger)error withMessage:(NSString *)message;
 @end
 
 @protocol GADFullScreenContentDelegate <NSObject>
 @optional
-- (void)adDidPresentFullScreenContent:(id)ad;
-- (void)ad:(id)ad didFailToPresentFullScreenContentWithError:(NSError *)error;
-- (void)adWillDismissFullScreenContent:(id)ad;
 - (void)adDidDismissFullScreenContent:(id)ad;
 @end
 
-static BOOL hooksInstalled = NO;
+// Track the last show delegate and placement so we can fire complete
+static id gLastShowDelegate = nil;
+static NSString *gLastPlacementId = nil;
 
-static void FireUnityAdsComplete(NSString *placementId, id<UnityAdsShowDelegate> delegate) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        if ([delegate respondsToSelector:@selector(unityAdsShowStart:)]) {
-            [delegate unityAdsShowStart:placementId];
-        }
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            if ([delegate respondsToSelector:@selector(unityAdsShowComplete:withFinish:)]) {
-                [delegate unityAdsShowComplete:placementId withFinish:kUnityShowCompletionStateCompleted];
-            }
-            NSLog(@"[ToSAdBypass] Unity Ads: fired Completed for placement: %@", placementId);
+static void FireComplete(void) {
+    id delegate = gLastShowDelegate;
+    NSString *placement = gLastPlacementId;
+    if (!delegate || !placement) return;
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if ([delegate respondsToSelector:@selector(unityAdsShowStart:)])
+            ((void(*)(id,SEL,NSString*))objc_msgSend)(delegate, @selector(unityAdsShowStart:), placement);
+
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if ([delegate respondsToSelector:@selector(unityAdsShowComplete:withFinish:)])
+                ((void(*)(id,SEL,NSString*,NSInteger))objc_msgSend)(delegate,
+                    @selector(unityAdsShowComplete:withFinish:), placement, 2);
+            gLastShowDelegate = nil;
+            gLastPlacementId = nil;
         });
     });
 }
 
-static void InstallHooks(void) {
-    if (hooksInstalled) return;
+static IMP orig_loadRequest = NULL;
 
-    BOOL anyInstalled = NO;
+// Hook WKWebView loadRequest to intercept Unity Ads webview navigation
+static id hooked_loadRequest(WKWebView *self, SEL _cmd, NSURLRequest *request) {
+    NSString *host = request.URL.host;
+    if ([host containsString:@"unity3d.com"] ||
+        [host containsString:@"unityads.unity3d.com"] ||
+        [host containsString:@"webview.unity3d.com"] ||
+        [host containsString:@"auction.unityads.unity3d.com"] ||
+        [host containsString:@"googlesyndication.com"] ||
+        [host containsString:@"doubleclick.net"]) {
 
-    // 1. Unity Ads 4.x
-    Class UAClass = NSClassFromString(@"UnityAds");
-    if (UAClass) {
-        SEL showSel = NSSelectorFromString(@"show:placementId:showDelegate:");
-        Method m1 = class_getClassMethod(UAClass, showSel);
-        if (m1) {
-            method_setImplementation(m1, imp_implementationWithBlock(
-                ^(id self, UIViewController *vc, NSString *placementId, id<UnityAdsShowDelegate> delegate) {
-                    NSLog(@"[ToSAdBypass] Intercepted show:placementId:showDelegate: (%@)", placementId);
-                    FireUnityAdsComplete(placementId, delegate);
+        // Block the webview from loading, fire reward immediately
+        FireComplete();
+
+        // Also fire GAD reward if this webview belongs to a GAD ad
+        // Walk up the responder chain to find the ad VC
+        UIResponder *responder = self;
+        while (responder) {
+            NSString *cn = NSStringFromClass([responder class]);
+            if ([cn containsString:@"GAD"] || [cn containsString:@"GMA"]) {
+                SEL rewardSel = NSSelectorFromString(@"userDidEarnRewardHandler");
+                if ([responder respondsToSelector:rewardSel]) {
+                    dispatch_block_t handler = ((id(*)(id,SEL))objc_msgSend)(responder, rewardSel);
+                    if (handler) handler();
                 }
-            ));
-            anyInstalled = YES;
+                SEL fsSel = NSSelectorFromString(@"fullScreenContentDelegate");
+                if ([responder respondsToSelector:fsSel]) {
+                    id fsDel = ((id(*)(id,SEL))objc_msgSend)(responder, fsSel);
+                    if ([fsDel respondsToSelector:@selector(adDidDismissFullScreenContent:)])
+                        [fsDel adDidDismissFullScreenContent:responder];
+                }
+                break;
+            }
+            responder = [responder nextResponder];
         }
 
-        SEL showOptSel = NSSelectorFromString(@"show:placementId:options:showDelegate:");
-        Method m2 = class_getClassMethod(UAClass, showOptSel);
-        if (m2) {
-            method_setImplementation(m2, imp_implementationWithBlock(
-                ^(id self, UIViewController *vc, NSString *placementId, id options, id<UnityAdsShowDelegate> delegate) {
-                    NSLog(@"[ToSAdBypass] Intercepted show:placementId:options:showDelegate: (%@)", placementId);
-                    FireUnityAdsComplete(placementId, delegate);
-                }
-            ));
-            anyInstalled = YES;
-        }
-
-        SEL loadSel = NSSelectorFromString(@"load:loadDelegate:");
-        Method m3 = class_getClassMethod(UAClass, loadSel);
-        if (m3) {
-            method_setImplementation(m3, imp_implementationWithBlock(
-                ^(id self, NSString *placementId, id<UnityAdsLoadDelegate> loadDelegate) {
-                    NSLog(@"[ToSAdBypass] Intercepted load:loadDelegate: (%@)", placementId);
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        if ([loadDelegate respondsToSelector:@selector(unityAdsAdLoaded:)]) {
-                            [loadDelegate unityAdsAdLoaded:placementId];
-                        }
-                    });
-                }
-            ));
-        }
-
-        SEL loadOptSel = NSSelectorFromString(@"load:options:loadDelegate:");
-        Method m4 = class_getClassMethod(UAClass, loadOptSel);
-        if (m4) {
-            method_setImplementation(m4, imp_implementationWithBlock(
-                ^(id self, NSString *placementId, id options, id<UnityAdsLoadDelegate> loadDelegate) {
-                    NSLog(@"[ToSAdBypass] Intercepted load:options:loadDelegate: (%@)", placementId);
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        if ([loadDelegate respondsToSelector:@selector(unityAdsAdLoaded:)]) {
-                            [loadDelegate unityAdsAdLoaded:placementId];
-                        }
-                    });
-                }
-            ));
-        }
-
-        NSLog(@"[ToSAdBypass] Unity Ads hooks installed");
+        return nil; // Block load
     }
 
-    // 2. Google Mobile Ads
-    Class GADClass = NSClassFromString(@"GADRewardedAd");
-    if (GADClass) {
-        SEL presentSel = NSSelectorFromString(@"presentFromRootViewController:userDidEarnRewardHandler:");
-        Method gm = class_getInstanceMethod(GADClass, presentSel);
-        if (gm) {
-            method_setImplementation(gm, imp_implementationWithBlock(
-                ^(id self, UIViewController *vc, dispatch_block_t rewardHandler) {
-                    NSLog(@"[ToSAdBypass] Intercepted GADRewardedAd present");
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        if (rewardHandler) {
-                            rewardHandler();
-                        }
-                        id<GADFullScreenContentDelegate> fsDelegate = nil;
-                        SEL fsSel = NSSelectorFromString(@"fullScreenContentDelegate");
-                        if ([self respondsToSelector:fsSel]) {
-                            fsDelegate = ((id(*)(id,SEL))objc_msgSend)(self, fsSel);
-                        }
-                        if ([fsDelegate respondsToSelector:@selector(adDidDismissFullScreenContent:)]) {
-                            [fsDelegate adDidDismissFullScreenContent:self];
-                        }
-                        NSLog(@"[ToSAdBypass] GADRewardedAd: fired reward handler");
-                    });
-                }
-            ));
-            anyInstalled = YES;
-        }
-        NSLog(@"[ToSAdBypass] GAD hooks installed");
-    }
+    return orig_loadRequest(self, _cmd, request);
+}
 
-    // 3. GMARewardedAdDelegateProxy
-    Class GMAProxyClass = NSClassFromString(@"GMARewardedAdDelegateProxy");
-    if (GMAProxyClass) {
-        SEL onCompleteSel = NSSelectorFromString(@"onAdComplete:network:rewarded:");
-        Method pm = class_getInstanceMethod(GMAProxyClass, onCompleteSel);
-        if (pm) {
-            IMP origOnComplete = method_getImplementation(pm);
-            method_setImplementation(pm, imp_implementationWithBlock(
-                ^(id self, id meta, NSString *network, BOOL rewarded) {
-                    NSLog(@"[ToSAdBypass] GMARewardedAdDelegateProxy forcing rewarded=YES");
-                    ((void(*)(id,SEL,id,NSString*,BOOL))origOnComplete)(self, onCompleteSel, meta, network, YES);
-                }
-            ));
-            anyInstalled = YES;
-        }
-        NSLog(@"[ToSAdBypass] GMA proxy hook installed");
-    }
+static IMP orig_presentVC = NULL;
 
-    if (anyInstalled) {
-        hooksInstalled = YES;
-        NSLog(@"[ToSAdBypass] All hooks installed successfully");
+static void hooked_presentVC(UIViewController *self, SEL _cmd, UIViewController *vc, BOOL animated, void(^completion)(void)) {
+    NSString *cn = NSStringFromClass([vc class]);
+    if ([cn containsString:@"UnityAds"] ||
+        [cn containsString:@"UADS"] ||
+        [cn containsString:@"GADFullScreen"] ||
+        [cn containsString:@"GMAFullScreen"]) {
+        if (completion) completion();
+        FireComplete();
+        return;
     }
+    orig_presentVC(self, _cmd, vc, animated, completion);
 }
 
 __attribute__((constructor))
 static void ToSAdBypassInit(void) {
-    NSLog(@"[ToSAdBypass] Constructor fired, waiting for Unity to load...");
+    // Hook WKWebView -loadRequest:
+    Class wkClass = NSClassFromString(@"WKWebView");
+    if (wkClass) {
+        Method m = class_getInstanceMethod(wkClass, @selector(loadRequest:));
+        if (m) {
+            orig_loadRequest = method_getImplementation(m);
+            method_setImplementation(m, (IMP)hooked_loadRequest);
+        }
+    }
 
+    // Hook UIViewController presentation as backup
+    Method pm = class_getInstanceMethod([UIViewController class],
+        @selector(presentViewController:animated:completion:));
+    orig_presentVC = method_getImplementation(pm);
+    method_setImplementation(pm, (IMP)hooked_presentVC);
+
+    // Hook Unity Ads show to capture delegate + placement before ad fires
     __block int attempts = 0;
     __block void (^tryHook)(void);
     tryHook = ^{
         Class UAClass = NSClassFromString(@"UnityAds");
-        Class GADClass = NSClassFromString(@"GADRewardedAd");
-        if (UAClass && GADClass) {
-            InstallHooks();
+        if (UAClass) {
+            SEL showSel = NSSelectorFromString(@"show:placementId:showDelegate:");
+            Method sm = class_getClassMethod(UAClass, showSel);
+            if (sm) {
+                method_setImplementation(sm, imp_implementationWithBlock(
+                    ^(id self, UIViewController *vc, NSString *placementId, id<UnityAdsShowDelegate> delegate) {
+                        gLastShowDelegate = delegate;
+                        gLastPlacementId = placementId;
+                        // Don't call original — WKWebView hook will fire complete
+                        // If webview hook misses for any reason, fire after 1s
+                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
+                            dispatch_get_main_queue(), ^{
+                            if (gLastShowDelegate) FireComplete();
+                        });
+                    }
+                ));
+            }
+
+            SEL showOptSel = NSSelectorFromString(@"show:placementId:options:showDelegate:");
+            Method sm2 = class_getClassMethod(UAClass, showOptSel);
+            if (sm2) {
+                method_setImplementation(sm2, imp_implementationWithBlock(
+                    ^(id self, UIViewController *vc, NSString *placementId, id options, id<UnityAdsShowDelegate> delegate) {
+                        gLastShowDelegate = delegate;
+                        gLastPlacementId = placementId;
+                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
+                            dispatch_get_main_queue(), ^{
+                            if (gLastShowDelegate) FireComplete();
+                        });
+                    }
+                ));
+            }
+
+            SEL loadSel = NSSelectorFromString(@"load:loadDelegate:");
+            Method lm = class_getClassMethod(UAClass, loadSel);
+            if (lm) {
+                method_setImplementation(lm, imp_implementationWithBlock(
+                    ^(id self, NSString *placementId, id<UnityAdsLoadDelegate> loadDelegate) {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            if ([loadDelegate respondsToSelector:@selector(unityAdsAdLoaded:)])
+                                [loadDelegate unityAdsAdLoaded:placementId];
+                        });
+                    }
+                ));
+            }
         } else if (attempts < 60) {
             attempts++;
-            NSLog(@"[ToSAdBypass] Classes not ready yet, attempt %d...", attempts);
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
                 dispatch_get_main_queue(), tryHook);
-        } else {
-            NSLog(@"[ToSAdBypass] Giving up after 60 attempts - UAClass=%d GADClass=%d",
-                UAClass != nil, GADClass != nil);
         }
     };
 
