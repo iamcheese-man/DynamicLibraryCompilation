3e30b2d697fa3979756a8722e10fe1d1ed801ee9
iamcheese-man <Ilyassmekarto9@gmail.com>
2026-03-21 21:52:09 +0000

    Update and rename FindASLR.mm to Findentry.mm

--- src/Findentry.mm
+++ src/Findentry.mm
@@ -3,6 +3,7 @@
 #include <mach/mach.h>
 #include <mach-o/dyld.h>
 #include <dlfcn.h>
+#include <string.h>
 
 static void showAlert(NSString *msg) {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -23,19 +24,39 @@ static void showAlert(NSString *msg) {
     });
 }
 
+// Luau lua_State magic bytes - first field is global_State pointer
+// We look for dlsym of known Luau exports in RobloxLib
 __attribute__((constructor))
 static void init(void) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)),
                    dispatch_get_main_queue(), ^{
-        uint32_t count = _dyld_image_count();
+        
+        void *robloxLib = dlopen("/private/var/mobile/Containers/Data/Application/7387409E-8C5F-4161-BB43-175BF6FEEE03/Documents/Applications/com.gloop.deltamobile.app/Frameworks/RobloxLib.framework/RobloxLib", RTLD_NOLOAD);
+        
         NSMutableString *result = [NSMutableString string];
-        for (uint32_t i = 0; i < count; i++) {
-            const char *name = _dyld_get_image_name(i);
-            if (name && strstr(name, "Roblox")) {
-                intptr_t slide = _dyld_get_image_vmaddr_slide(i);
-                [result appendFormat:@"Image: %s\nSlide: 0x%lx\n", name, slide];
+        
+        if (robloxLib) {
+            [result appendString:@"RobloxLib handle: OK\n"];
+            
+            // Try common Luau exports
+            const char *syms[] = {
+                "lua_pushstring", "lua_getfield", "lua_pcall",
+                "luaL_newstate", "lua_newstate", "lua_getglobal",
+                "luaL_openlibs", "lua_call", "lua_settop",
+                "rbx_lua_pushstring", "rbx_lua_pcall",
+                NULL
+            };
+            
+            for (int i = 0; syms[i]; i++) {
+                void *sym = dlsym(robloxLib, syms[i]);
+                if (sym) [result appendFormat:@"FOUND: %s = %p\n", syms[i], sym];
             }
+            
+            if (result.length < 20) [result appendString:@"No named Lua exports found"];
+        } else {
+            [result appendFormat:@"dlopen failed: %s", dlerror()];
         }
-        showAlert(result.length ? result : @"No Roblox image found");
+        
+        showAlert(result);
     });
 }
